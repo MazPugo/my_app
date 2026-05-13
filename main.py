7ce46e252e78364e3fb05f2aff693b40af7deaa6
MazPugo <maz.pugo@gmail.com>
2026-05-13 07:19:49 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello, World!"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+
+
+
